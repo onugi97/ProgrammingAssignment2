f80fe2335e2302b0e141b8ea1a4160c888c17fe3
onook oh <onookoh@gmail.com>
2014-05-17 18:06:06 +0100

    makeCacheMatrix updated
    makeCacheMatrix has been updated as Programming Assignment 2 for R-Programming Coursera course.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,41 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL # inverse matrix
+      
+      setmatrix <-function(y){
+            if (is.matrix(y) == TRUE) x<<-y
+            else print("Input is not a matrix")
+            m<<-NULL
+      }
+      
+      getmatrix<-function() x
+      
+      setmatrixinverse <-function(y){
+            if(is.matrix(y) == TRUE) m <<- y
+            else print("Inpput is not a matrix")
+      }
+      
+      getmatrixinverse<- function() m 
+      
+      list (getmatrix = getmatrix, setmatrix = setmatrix, 
+            getinverse = getmatrixinverse, setinverse = setmatrixinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+      
+      matrix_inverse<-x$getinverse()
+      if (!is.null(matrix_inverse)) {
+            print("getting inverse from cache")
+            return(matrix_inverse)
+      }
+      
+      matrix_value = x$getmatrix()
+      matrix_inverse = solve(matrix_value)
+      x$setinverse(matrix_inverse)
+      matrix_inverse        ## Return a matrix that is the inverse of 'x'
 }
